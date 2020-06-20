43da25f34300db9988623fe6938cbede5b9b9ffb
Jeet Dholakia <onemediasoft@gmail.com>
2020-06-20 14:19:47 +0530

    added background thread files

---
+++ app/src/main/java/dev/jeetdholakia/backgroundthreads/BackgroundThreadActivity.kt
@@ -0,0 +1,12 @@
+package dev.jeetdholakia.backgroundthreads
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.jeetdholakia.servicesandbackgroundtasks.R
+
+class BackgroundThreadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_background_thread)
+    }
+}
